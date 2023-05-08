eaa6409ed702e66ff2db4915867319c39d98ec93
omid <omid.mesgarha@gmail.com>
2023-05-08 16:52:57 +0430

    add config reader file

---
+++ pkg/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+func GetConfigs()(map[string]string, error){
+	f, err := os.ReadFile("pkg/config/env.yml")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var data map[string]string
+
+    err = yaml.Unmarshal(f, &data)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(data)
+    return data, nil
+}
