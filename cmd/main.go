7dc7b4b4be3e2809098a0cadf6713dcad2466149
omid <omid.mesgarha@gmail.com>
2024-12-03 00:50:39 +0330

    add profiler

--- cmd/main.go
+++ cmd/main.go
@@ -11,11 +11,10 @@ import (
 
 func main(){
 	err := sentry.Init(sentry.ClientOptions{
+		EnableTracing: true,
 		Dsn: "https://4c2fbcc74e453c8e558c3b952b494c59@sentry.hamravesh.com/7641",
-		// Set TracesSampleRate to 1.0 to capture 100%
-		// of transactions for performance monitoring.
-		// We recommend adjusting this value in production,
 		TracesSampleRate: 1.0,
+		ProfilesSampleRate: 1.0,
 	  })
 	  if err != nil {
 		log.Fatalf("sentry.Init: %s", err)
